61834c9606dd842fc814503b5bc55bc91421ed6f
春日優作 <yuayanao0105@gmail.com>
2020-03-22 04:01:16 +0000

    とりあえずやってみた

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+class PagesController < ApplicationController
+  def index
+  end
+  def help
+  end
+end
